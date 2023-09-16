386ba3a08afdfa74c0edf12295bd6c7942df380d
Serkan <srkntrkr050@gmail.com>
2023-09-16 23:04:34 +0300

    seko

---
+++ android/app/src/main/kotlin/com/example/sekom_coffee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sekom_coffee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/cafe_model.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Cafe {
+  final String imageUrl, name, address;
+  Cafe({
+    required this.name,
+    required this.imageUrl,
+    required this.address,
+  });
+}
+
+List<Cafe> cafeList = [
+  Cafe(
+    name: "Nevşehir Merkez",
+    imageUrl: "star.jpg",
+    address: "Nevşehir Merkez",
+  ),
+  Cafe(
+    name: "Nevşehir Merkez",
+    imageUrl: "star.jpg",
+    address: "Nevşehir Merkez",
+  ),
+  Cafe(
+    name: "Nevşehir Merkez",
+    imageUrl: "star.jpg",
+    address: "Nevşehir Merkez",
+  ),
+  Cafe(
+    name: "Nevşehir Merkez",
+    imageUrl: "star.jpg",
+    address: "Nevşehir Merkez",
+  ),
+  Cafe(
+    name: "Nevşehir Merkez",
+    imageUrl: "star.jpg",
+    address: "Nevşehir Merkez",
+  ),
+];
